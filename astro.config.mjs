c6a87105fca086b0eaf21d991efde9cd1d5cc2ae
Willians Pantoja <willianspantojaantay@gmail.com>
2026-09-14 01:05:56 -0500

    Verificacion google console

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config';
 import tailwindv4 from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://gethsemani.vercel.app',
   output: 'server',
+
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
+
   vite: {
     plugins: [tailwindv4()],
   },
+
+  integrations: [sitemap()],
 });
